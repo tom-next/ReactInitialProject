b86a5f4f90e9ace774c71e566191fc5ec1acdd41
tom <tom.next.miao@gmail.com>
2018-11-19 18:52:09 +0800

    初始化项目，配置 webpack,生产环境和开发环境配置，图片资源加载

---
+++ config/webpack.base.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const merge = require("webpack-merge");
+
+// 将生成的css提取到一个名为main.css的单独模块中  
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Add this in top
+module.exports = env => {
+  const { PLATFORM, VERSION } = env;
+  return merge([
+      {
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader'
+              }
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            }
+          ]
+        },
+        plugins: [
+          new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+          }),
+          new webpack.DefinePlugin({ 
+            'process.env.VERSION': JSON.stringify(env.VERSION),
+            'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+          }),
+          new CopyWebpackPlugin([ { from: 'src/static' } ]),
+        ],
+    }
+  ])
+};
