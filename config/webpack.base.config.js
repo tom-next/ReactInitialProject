48bcf7643c7cfa16c25b57fc1655f750fcec0a15
Tom <tom526019128@foxmail.com>
2018-11-20 00:48:43 +0800

    支持 css modules

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,15 +1,18 @@
 const webpack = require('webpack');
 const merge = require("webpack-merge");
-
+const path = require("path")
 // 将生成的css提取到一个名为main.css的单独模块中  
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin'); // Add this in top
+
+const APP_DIR = path.resolve(__dirname, '../src'); 
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
       {
+        entry: ['@babel/polyfill', APP_DIR], 
         module: {
           rules: [
             {
@@ -22,11 +25,45 @@ module.exports = env => {
             {
               test: /\.scss$/,
               use: [
-                PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                'css-loader',
+                  PLATFORM === 'production' ?  new MiniCssExtractPlugin({
+                      filename: '[name].css',
+                      chunkFilename: '[id].css'
+                  }) : 'style-loader',
+                  {
+                      loader: "css-loader",
+                      options: {
+                          sourceMap: true,
+                          modules: true,
+                          localIdentName: "[local]___[hash:base64:5]"
+                      }
+                  },
                 'sass-loader'
               ]
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,    // 小于10000kb自动转base64
+                    name: 'static/images/[name].[hash:7].[ext]'
+                }
+            },
+              {
+                  test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                  loader: 'url-loader',
+                  options: {
+                      limit: 10000,
+                      name: 'static/media/[name].[hash:7].[ext]'
+                  }
+              },
+              {
+                  test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                  loader: 'url-loader',
+                  options: {
+                      limit: 10000,
+                      name: 'static/font/[name].[hash:7].[ext]'
+                  }
+              },
           ]
         },
         plugins: [
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './myStyles.scss';
-
+import * as  styles from './myStyles.scss';
+import headerUrl from './static/images/header.jpeg'
+ 
 const App = () => {
   return (
-    <div className="app">
-      <img alt="header" src="/dist/images/header.jpeg" className="app-header" />
+      <div className={styles.app}>
+          <img alt="header" src={headerUrl} className="app-header" />
       <p>
         We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
         have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
